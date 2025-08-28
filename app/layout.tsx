5e329815e253b5687632cc5d7c8cdc77870688d1
moetazaneta <teqqmu@gmail.com>
2025-08-28 19:56:16 +0600

    feat: change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type {Metadata} from "next"
 import {Geist, Geist_Mono} from "next/font/google"
+import localFont from "next/font/local"
+
 import "./globals.css"
 import {ConvexAuthNextjsServerProvider} from "@convex-dev/auth/nextjs/server"
 import ConvexClientProvider from "@/components/ConvexClientProvider"
 import {Toaster} from "@/components/ui/sonner"
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+const martianGrotesk = localFont({
+	src: "./fonts/MartianGrotesk.woff2",
+	variable: "--font-martian-grotesk",
 })
 
 const geistMono = Geist_Mono({
@@ -32,10 +34,10 @@ export default function RootLayout({
 		<ConvexAuthNextjsServerProvider>
 			<html lang="en">
 				<body
-					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+					className={`${martianGrotesk.variable} ${geistMono.variable} antialiased`}
 				>
 					<ConvexClientProvider>
-						<div className="flex flex-col items-center min-h-screen w-screen p-8">
+						<div className="flex flex-col items-center h-screen w-screen p-8">
 							{children}
 						</div>
 						<Toaster />
